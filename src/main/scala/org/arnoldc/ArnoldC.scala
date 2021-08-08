fa048df2ad314cc7a20b444a39badb3eafa62a1c
long-long-double <73804770+long-long-double@users.noreply.github.com>
2021-08-08 19:23:52 +0000

    v2.4.4

--- src/main/scala/org/arnoldc/ArnoldC.scala
+++ src/main/scala/org/arnoldc/ArnoldC.scala
@@ -34,4 +34,5 @@ object ArnoldC {
     case _ => throw new RuntimeException("WHAT THE FUCK DID I DO WRONG!")
   }
 
-  def getCommandFromArgs(args:Array[String])
+  def getCommandFromArgs(args:Array[String]):String = args.length match {
+    case 2 => args(0)
