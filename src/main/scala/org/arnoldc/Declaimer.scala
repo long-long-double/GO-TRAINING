4aeeb53ae5eb3505f6244afd2ae3b41b625c9d51
long-long-double <73804770+long-long-double@users.noreply.github.com>
2021-09-22 18:04:51 +0000

    new tools

--- src/main/scala/org/arnoldc/Declaimer.scala
+++ src/main/scala/org/arnoldc/Declaimer.scala
@@ -27,3 +27,57 @@ object Declaimer {
     case RootNode(methods) => methods.map(m => declaim(m))
     case MainMethodNode(stmts) =>
       say(p.BeginMain)
+      stmts foreach declaim
+      say(p.EndMain)
+    case MethodNode(name, args, ret, stmts) =>
+      say(s"${p.DeclareMethod} $name")
+      args.foreach(a => say(s"${p.MethodArguments} ${a.variableName}"))
+      if (ret) say(p.NonVoidMethod);
+      stmts.map(s => declaim(s))
+      say(p.EndMethodDeclaration)
+
+    case AssignVariableNode(name, expr) =>
+      say(s"${p.AssignVariable} $name")
+      say(p.SetValue)
+      declaim(expr)
+      say(p.EndAssignVariable)
+    case PrintNode(what) =>
+      say(p.Print)
+      declaim(what)
+    case DeclareIntNode(name, value) =>
+      say(s"${p.DeclareInt} $name")
+      say(s"${p.SetInitialValue} $value")
+    case ConditionNode(condition, ifStmts, elseStmts) =>
+      say(p.If)
+      declaim(condition)
+      ifStmts foreach declaim
+      if (elseStmts.nonEmpty) {
+        say(p.Else)
+        elseStmts foreach declaim
+      }
+      say(p.EndIf)
+    case WhileNode(condition, stmts) =>
+      say(p.While)
+      declaim(condition)
+      stmts foreach declaim
+      say(p.EndWhile)
+    case CallMethodNode(variable, name, args) =>
+      if (variable.nonEmpty) {
+        say(s"${p.AssignVariableFromMethodCall} $variable")
+      }
+      say(s"${p.CallMethod} $name")
+      args foreach declaim
+    case CallReadMethodNode(variable) =>
+      say(s"${p.Read} $variable")
+    case ReturnNode(expr) =>
+      say(p.Return)
+      expr.map { x => declaim(x) }
+
+    case AndNode(expr1, expr2) =>
+      declaim(expr1)
+      say(p.And)
+      declaim(expr2)
+    case OrNode(expr1, expr2) =>
+      declaim(expr1)
+      say(p.Or)
+      declaim(expr2)
