8af9b44b943dad1e11b2f177d398811237adc025
long-long-double <73804770+long-long-double@users.noreply.github.com>
2021-10-21 06:24:08 +0000

    initial working commit

--- src/main/scala/org/arnoldc/ast/AstNode.scala
+++ src/main/scala/org/arnoldc/ast/AstNode.scala
@@ -2,4 +2,5 @@ package org.arnoldc.ast
 
 import org.objectweb.asm.{MethodVisitor, ClassWriter}
 import org.objectweb.asm.Opcodes._
-import org.objectweb.asm
+import org.objectweb.asm.commons.{GeneratorAdapter, Method}
+import org.arnol
