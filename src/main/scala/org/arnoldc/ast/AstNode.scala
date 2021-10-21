fabcb3dc1fa124c2c1ba55fb51037c62caced9a1
long-long-double <73804770+long-long-double@users.noreply.github.com>
2021-10-21 12:13:27 +0000

    :wrench:

--- src/main/scala/org/arnoldc/ast/AstNode.scala
+++ src/main/scala/org/arnoldc/ast/AstNode.scala
@@ -3,4 +3,7 @@ package org.arnoldc.ast
 import org.objectweb.asm.{MethodVisitor, ClassWriter}
 import org.objectweb.asm.Opcodes._
 import org.objectweb.asm.commons.{GeneratorAdapter, Method}
-import org.arnol
+import org.arnoldc.SymbolTable
+
+abstract class AstNode {
+ 
