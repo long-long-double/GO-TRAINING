826e12c7b1c6235ea85dd2e9e058114cf3a8a291
long-long-double <73804770+long-long-double@users.noreply.github.com>
2021-10-24 07:22:03 +0000

    update test script

--- src/main/scala/org/arnoldc/ast/CallReadMethodNode.scala
+++ src/main/scala/org/arnoldc/ast/CallReadMethodNode.scala
@@ -2,4 +2,6 @@ package org.arnoldc.ast
 
 import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes._
-import org.arnoldc.Symbo
+import org.arnoldc.SymbolTable
+
+case class CallReadMethodNode(r
