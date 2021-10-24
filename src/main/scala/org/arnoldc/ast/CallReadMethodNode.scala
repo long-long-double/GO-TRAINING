9b812d7975cf14b3647237eb34c399f304521581
long-long-double <73804770+long-long-double@users.noreply.github.com>
2021-10-24 14:10:47 +0000

    v2.5.5

--- src/main/scala/org/arnoldc/ast/CallReadMethodNode.scala
+++ src/main/scala/org/arnoldc/ast/CallReadMethodNode.scala
@@ -4,4 +4,5 @@ import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes._
 import org.arnoldc.SymbolTable
 
-case class CallReadMethodNode(r
+case class CallReadMethodNode(returnVar: String) extends StatementNode{
+  def generat
