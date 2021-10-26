4664278ec89cacd6dedbf7d561b7421cf49e4e61
long-long-double <73804770+long-long-double@users.noreply.github.com>
2021-10-26 20:15:27 +0000

    Regenerate gemspec for version 0.6.1

--- src/main/scala/org/arnoldc/ast/CallReadMethodNode.scala
+++ src/main/scala/org/arnoldc/ast/CallReadMethodNode.scala
@@ -5,4 +5,5 @@ import org.objectweb.asm.Opcodes._
 import org.arnoldc.SymbolTable
 
 case class CallReadMethodNode(returnVar: String) extends StatementNode{
-  def generate(mv: MethodVisitor, symbolTable: SymbolTabl
+  def generate(mv: MethodVisitor, symbolTable: SymbolTable) = {
+    mv.visitTypeInsn(NEW, "java/util/Scanne
