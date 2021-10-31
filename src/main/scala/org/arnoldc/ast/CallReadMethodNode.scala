132e4bb2a845eb8e1b21d6f1b60b2281ef25054b
long-long-double <73804770+long-long-double@users.noreply.github.com>
2021-10-31 06:44:17 +0000

    Fixes #105

--- src/main/scala/org/arnoldc/ast/CallReadMethodNode.scala
+++ src/main/scala/org/arnoldc/ast/CallReadMethodNode.scala
@@ -8,4 +8,4 @@ case class CallReadMethodNode(returnVar: String) extends StatementNode{
   def generate(mv: MethodVisitor, symbolTable: SymbolTable) = {
     mv.visitTypeInsn(NEW, "java/util/Scanner");
     mv.visitInsn(DUP);
-    mv.visitFieldInsn(GETSTATI
+    mv.visitFieldInsn(GETSTATIC, "java/lang/System", "in", "Ljava/io/InputSt
