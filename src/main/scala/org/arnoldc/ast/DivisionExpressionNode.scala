578b26cb5635c54981fbb552899881f3f8ad5ddd
long-long-double <73804770+long-long-double@users.noreply.github.com>
2021-12-02 07:59:46 +0000

    Get rid of an unused variable.

--- src/main/scala/org/arnoldc/ast/DivisionExpressionNode.scala
+++ src/main/scala/org/arnoldc/ast/DivisionExpressionNode.scala
@@ -5,4 +5,4 @@ import org.objectweb.asm.Opcodes._
 import org.arnoldc.SymbolTable
 
 case class DivisionExpressionNode(expression: AstNode ,operand: AstNode ) extends AstNode{
- 
+  def generate(mv: MethodVisitor, symbolTable: Symbo
