0be8b45aa67d64d1dbfbac825ec167f6fd6079cc
long-long-double <73804770+long-long-double@users.noreply.github.com>
2022-01-12 22:15:40 +0000

    use sudo

--- src/main/scala/org/arnoldc/ast/EqualToNode.scala
+++ src/main/scala/org/arnoldc/ast/EqualToNode.scala
@@ -18,4 +18,5 @@ case class EqualToNode(operand1: AstNode, operand2: AstNode) extends ExpressionN
     mv.visitLabel(notEqual)
     mv.visitFrame(F_FULL, symbolTable.size(), symbolTable.getStackFrame, 0, null)
     mv.visitInsn(ICONST_0)
-    mv.visitLabel(
+    mv.visitLabel(conclude)
+    mv.visitFrame(F_SAME1, 0, n
