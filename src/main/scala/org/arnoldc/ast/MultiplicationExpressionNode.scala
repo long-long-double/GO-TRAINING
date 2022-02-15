104980ff820f8d4a1d2c9999c5f07d597bf07aab
long-long-double <73804770+long-long-double@users.noreply.github.com>
2022-02-15 07:51:46 +0000

    Version bump to 2.0.2

--- src/main/scala/org/arnoldc/ast/MultiplicationExpressionNode.scala
+++ src/main/scala/org/arnoldc/ast/MultiplicationExpressionNode.scala
@@ -4,4 +4,5 @@ import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes._
 import org.arnoldc.SymbolTable
 
-case class MultiplicationExpressionNode(expression: AstN
+case class MultiplicationExpressionNode(expression: AstNode ,operand: AstNode ) extends AstNode{
+  def generate(mv: M
