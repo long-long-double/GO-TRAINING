56e46348d07f2da789c121ac007aef64e1aa181c
long-long-double <73804770+long-long-double@users.noreply.github.com>
2022-02-27 22:17:22 +0000

    well

--- src/main/scala/org/arnoldc/ast/OrNode.scala
+++ src/main/scala/org/arnoldc/ast/OrNode.scala
@@ -2,4 +2,7 @@ package org.arnoldc.ast
 
 import org.objectweb.asm.{Opcodes, MethodVisitor, Label}
 import org.objectweb.asm.Opcodes._
-import org.arnoldc.Symb
+import org.arnoldc.SymbolTable
+
+
+case class OrNode(expression: Ast
