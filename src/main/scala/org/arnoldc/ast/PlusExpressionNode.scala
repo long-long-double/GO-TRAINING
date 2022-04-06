6d0b502b681da61f31cb93b5e18d6717b8087de1
long-long-double <73804770+long-long-double@users.noreply.github.com>
2022-04-06 09:08:24 +0000

    Update git.html

--- src/main/scala/org/arnoldc/ast/PlusExpressionNode.scala
+++ src/main/scala/org/arnoldc/ast/PlusExpressionNode.scala
@@ -4,4 +4,5 @@ import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes._
 import org.arnoldc.SymbolTable
 
-case class PlusExpressionNode(expression: AstNode ,operand: As
+case class PlusExpressionNode(expression: AstNode ,operand: AstNode ) extends AstNode{
+  def generate(mv: MethodVis
