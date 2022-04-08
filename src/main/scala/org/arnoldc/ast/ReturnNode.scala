453ffb6583c7f25b36db0200e48aee9536ae89b7
long-long-double <73804770+long-long-double@users.noreply.github.com>
2022-04-08 15:55:56 +0000

    New translations en.json (Hungarian) [ci skip] Signed-off-by: Björn Kimminich <bjoern.kimminich@gmx.de>

--- src/main/scala/org/arnoldc/ast/ReturnNode.scala
+++ src/main/scala/org/arnoldc/ast/ReturnNode.scala
@@ -2,4 +2,5 @@ package org.arnoldc.ast
 
 import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes._
-import org.arnoldc
+import org.arnoldc.SymbolTable
+import org.parboiled.errors.ParsingExcept
