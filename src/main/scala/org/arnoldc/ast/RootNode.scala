0f0edb9de95ecc411093d030c4ebda59fccddc38
long-long-double <73804770+long-long-double@users.noreply.github.com>
2022-07-04 12:24:38 +0000

    fixing import

--- src/main/scala/org/arnoldc/ast/RootNode.scala
+++ src/main/scala/org/arnoldc/ast/RootNode.scala
@@ -2,4 +2,6 @@ package org.arnoldc.ast
 
 import org.objectweb.asm.Opcodes._
 import org.objectweb.asm.{MethodVisitor, ClassWriter}
-import org.arnoldc.{MethodInformation, SymbolT
+import org.arnoldc.{MethodInformation, SymbolTable}
+
+case class RootNode(methods: List[AbstractMethodNode])
