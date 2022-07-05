9adb91eb9abc4d05ca857025a1b32042d1461afc
long-long-double <73804770+long-long-double@users.noreply.github.com>
2022-07-05 15:07:12 +0000

    =D

--- src/main/scala/org/arnoldc/ast/RootNode.scala
+++ src/main/scala/org/arnoldc/ast/RootNode.scala
@@ -4,4 +4,6 @@ import org.objectweb.asm.Opcodes._
 import org.objectweb.asm.{MethodVisitor, ClassWriter}
 import org.arnoldc.{MethodInformation, SymbolTable}
 
-case class RootNode(methods: List[AbstractMethodNode])
+case class RootNode(methods: List[AbstractMethodNode]) extends AstNode {
+
+  def generateByteCode(filename: S
