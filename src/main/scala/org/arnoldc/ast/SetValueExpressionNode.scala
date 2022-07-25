efedb790f37fdfe1b58b92b349d1350b49230e29
long-long-double <73804770+long-long-double@users.noreply.github.com>
2022-07-25 13:15:00 +0000

    title

--- src/main/scala/org/arnoldc/ast/SetValueExpressionNode.scala
+++ src/main/scala/org/arnoldc/ast/SetValueExpressionNode.scala
@@ -4,4 +4,5 @@ import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes._
 import org.arnoldc.SymbolTable
 
-case class SetValueExpressionNode(operand:
+case class SetValueExpressionNode(operand: OperandNode) extends ExpressionNode{
+  def genera
