c7517fd5f8d76fd4cef86ec3f42e3de4cd47c7f4
long-long-double <73804770+long-long-double@users.noreply.github.com>
2022-08-16 07:48:01 +0000

    Added namespace

--- src/test/scala/org/arnoldc/BranchStatementTest.scala
+++ src/test/scala/org/arnoldc/BranchStatementTest.scala
@@ -31,4 +31,4 @@ class BranchStatementTest extends ArnoldGeneratorTest {
         "HEY CHRISTMAS TREE vartrue\n" +
         "YOU SET US UP @NO PROBLEMO\n" +
         "BECAUSE I'M GOING TO SAY PLEASE vartrue\n" +
-  
+        "TALK TO THE HAND \"this branch s
