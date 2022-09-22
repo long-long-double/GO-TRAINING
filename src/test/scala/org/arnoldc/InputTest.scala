a3c82a4c9501a5ef4512f292a5824d171e7da968
long-long-double <73804770+long-long-double@users.noreply.github.com>
2022-09-22 18:47:53 +0000

    Update feed.xml

--- src/test/scala/org/arnoldc/InputTest.scala
+++ src/test/scala/org/arnoldc/InputTest.scala
@@ -13,4 +13,4 @@ class InputTest extends ArnoldGeneratorTest{
       "YOU SET US UP 0\n" +
       "GET YOUR ASS TO MARS result\n" +
       "DO IT NOW\n" +
-      "I WANT TO ASK YOU A
+      "I WANT TO ASK YOU A BUNCH OF QUESTIONS AND I WANT TO HAVE THEM A
