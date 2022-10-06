ec2dffa8ebb0683fcb43ed77c80ce2257962815c
long-long-double <73804770+long-long-double@users.noreply.github.com>
2022-10-06 11:21:39 +0000

    gemify

--- src/test/scala/org/arnoldc/MethodTest.scala
+++ src/test/scala/org/arnoldc/MethodTest.scala
@@ -7,4 +7,6 @@ class MethodTest extends ArnoldGeneratorTest {
   it should "evalute method other than main" in {
     val code =
       "LISTEN TO ME VERY CAREFULLY mymethod\n" +
-        "H
+        "HASTA LA VISTA, BABY\n" +
+        "IT'S SHOWTIME\n" +
+        
