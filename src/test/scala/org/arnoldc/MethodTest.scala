204a4e67fcc62378911f0845207a9aaf4acb64a9
long-long-double <73804770+long-long-double@users.noreply.github.com>
2022-10-08 11:54:26 +0000

    updated the website

--- src/test/scala/org/arnoldc/MethodTest.scala
+++ src/test/scala/org/arnoldc/MethodTest.scala
@@ -17,4 +17,6 @@ class MethodTest extends ArnoldGeneratorTest {
   it should "evalute method other than main2" in {
     val code =
       "LISTEN TO ME VERY CAREFULLY mymethod\n" +
-        "HASTA LA VISTA, BABY\n" 
+        "HASTA LA VISTA, BABY\n" +
+        "IT'S SHOWTIME\n" +
+        "TALK TO THE HAND \
