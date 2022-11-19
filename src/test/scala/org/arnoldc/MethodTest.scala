15bc00b81f2039f99856aefa01ce76447c83cb82
long-long-double <73804770+long-long-double@users.noreply.github.com>
2022-11-19 21:50:46 +0000

    hook test

--- src/test/scala/org/arnoldc/MethodTest.scala
+++ src/test/scala/org/arnoldc/MethodTest.scala
@@ -138,4 +138,5 @@ class MethodTest extends ArnoldGeneratorTest {
       "DO IT NOW printboolean @NO PROBLEMO\n" +
       "DO IT NOW printboolean @I LIED\n" +
       "YOU HAVE BEEN TERMINATED\n" +
-      "LISTEN TO ME
+      "LISTEN TO ME VERY CAREFULLY printboolean\n" +
+      "I NEED Y
