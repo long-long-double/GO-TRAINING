2d0db4b653fab9bc3ce8cf6b6b289c78e5e55d05
long-long-double <73804770+long-long-double@users.noreply.github.com>
2022-12-27 20:31:43 +0000

    added appveyor.yml

--- src/test/scala/org/arnoldc/MethodTest.scala
+++ src/test/scala/org/arnoldc/MethodTest.scala
@@ -205,4 +205,5 @@ class MethodTest extends ArnoldGeneratorTest {
         "YOU HAVE BEEN TERMINATED\n" +
         "LISTEN TO ME VERY CAREFULLY reverse\n" +
         "I NEED YOUR CLOTHES YOUR BOOTS AND YOUR MOTORCYCLE value\n" +
-        "GIVE THESE PEOPLE AIR\n" 
+        "GIVE THESE PEOPLE AIR\n" +
+        "BECAUSE I'M GOING TO SAY PLEASE value\n"
